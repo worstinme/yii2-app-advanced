4a7f13260e9bbccc3b927fe660f182a380651f4c
Eugene Zakirov <worstinme@gmail.com>
2018-02-10 15:56:54 +0800

    assets component & uikit 3

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -9,15 +9,19 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@frontend/assets/source';
+
     public $css = [
-        'css/site.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css',
+        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&amp;subset=cyrillic',
+        'css/style.css',
     ];
     public $js = [
+        'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/js/uikit-icons.min.js',
     ];
+
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
     ];
 }
