1386617dd4d140e098da537054cbf9ba777b4f15
Eugene Zakirov <worstinme@gmail.com>
2018-04-20 16:48:07 +0800

    cart configs

--- common/config/main.php
+++ common/config/main.php
@@ -18,6 +18,20 @@ return [
         'widgets'=>[
             'class'=>'\worstinme\widgets\Component',
         ],
+        'cart' => [
+            'class' => '\worstinme\cart\Component',
+            'min_sum_to_order' => 1000,
+            'relationNameField' => function ($model) {
+                return $model->element_name;
+            },
+            'relationImageField' => function ($model) {
+                if (isset($model->element_images[0])) {
+                    return \worstinme\zoo\helpers\ImageHelper::thumbnailFileUrl(Yii::getAlias('@frontend/web') . $model->element_images[0]['source'], 60, 40);
+                }
+                return null;
+            },
+            'relationCategoryField' => 'parentCategory.name'
+        ],
         'i18n' => [
             'translations' => [
                 'app*' => [
