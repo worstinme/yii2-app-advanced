556fd1c6ed76ca2e7d985fcf42bd2161cd16d0a2
Eugene Zakirov <worstinme@gmail.com>
2018-04-20 16:40:35 +0800

    fontawesome

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -14,6 +14,7 @@ class AppAsset extends AssetBundle
     public $css = [
         'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css',
         'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&amp;subset=cyrillic',
+        'https://use.fontawesome.com/releases/v5.0.9/css/all.css',
         'css/style.css',
     ];
     public $js = [
