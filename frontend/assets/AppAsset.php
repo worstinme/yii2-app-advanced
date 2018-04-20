378e578a6899eb5805e483271dc59a2edfa5b106
Eugene Zakirov <worstinme@gmail.com>
2018-04-20 16:51:49 +0800

    fontawesome

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,8 @@ class AppAsset extends AssetBundle
     public $sourcePath = '@frontend/assets/source';
     public $css = [
         'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.39/css/uikit.min.css',
-        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&amp;subset=cyrillic',
+        'https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700,900&amp;subset=cyrillic',
+        'https://use.fontawesome.com/releases/v5.0.9/css/all.css',
         'css/style.css',
     ];
     public $js = [
